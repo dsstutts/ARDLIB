7d48d9cb341291549f4b8edf23839635aebc8183
LinuxMercedes <LinuxMercedes@gmail.com>
2013-04-28 22:20:17 -0500

    Much better example code

--- lib/test.ino
+++ lib/test.ino
@@ -1,7 +1,9 @@
 #include "DataLogger.h"
 
 int iset;
+int run = 1;
 float fset;
+float angle = 0;
 
 DataLogger dl;
 
@@ -9,14 +11,21 @@ void setup()
 {
 	Serial.begin(115200);
 	dl.addSeries(0, "asdf");
+	dl.addSeries(1, "intsettingstream");
+	dl.addSeries(2, "sine");
 	dl.addIntSetting("intsetting", &iset);
 	dl.addFloatSetting("floatsetting", &fset);
+	dl.addIntSetting("run", &run);
 }
 
 void loop() 
 {
-	static int x = 0;
-	x++;
-	//dl.write(0, 4.5);
+	if(run)
+	{
+		dl.write(0, 4.5);
+		dl.write(1, iset);
+		dl.write(2, sin(angle));
+		angle += 0.1;
+	}
 }
 
